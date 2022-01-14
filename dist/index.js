9805266bc52184216847b38554d003fe71da16ee
Sean Metcalf <sean@seanmart.com>
2022-01-14 16:12:49 -0500

    updates

--- dist/index.js
+++ dist/index.js
@@ -8479,27 +8479,23 @@ async function getWeather(){
   let api_key = (0,core.getInput)('API_KEY')
   let coordinates = JSON.parse((0,core.getInput)('COORDINATES'))
   let exclude = (0,core.getInput)('EXCLUDE')
-  let arr = []
+  let output = {}
+  let count = 0
 
   if (!Array.isArray(coordinates)) coordinates = [coordinates]
 
   for (const c of coordinates){
     let lon = c.longitude || null
     let lat = c.latitude || null
-    let id = c.id || null
+    let id = c.id || count
     let obj = {}
     if (lon && lat){
       let query = `lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${api_key}`
       let res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?${query}`)
-      if (res.ok){
-        obj.data = await res.json()
-        if (id) obj.id = id
-        arr.push(obj)
-      }
+      if (res.ok) output[id] = await res.json()
     }
+    count++
   }
-
-  let output = arr.length == 1 ? arr[0] : arr
 
   console.log('output',output)
   ;(0,core.setOutput)('DATA', JSON.stringify(output));
--- index.js
+++ index.js
@@ -6,27 +6,23 @@ async function getWeather(){
   let api_key = getInput('API_KEY')
   let coordinates = JSON.parse(getInput('COORDINATES'))
   let exclude = getInput('EXCLUDE')
-  let arr = []
+  let output = {}
+  let count = 0
 
   if (!Array.isArray(coordinates)) coordinates = [coordinates]
 
   for (const c of coordinates){
     let lon = c.longitude || null
     let lat = c.latitude || null
-    let id = c.id || null
+    let id = c.id || count
     let obj = {}
     if (lon && lat){
       let query = `lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${api_key}`
       let res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?${query}`)
-      if (res.ok){
-        obj.data = await res.json()
-        if (id) obj.id = id
-        arr.push(obj)
-      }
+      if (res.ok) output[id] = await res.json()
     }
+    count++
   }
-
-  let output = arr.length == 1 ? arr[0] : arr
 
   console.log('output',output)
   setOutput('DATA', JSON.stringify(output));
