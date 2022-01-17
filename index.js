ea56b266e49a4c7073b84eb7f18e5e949d9a35c3
Sean Metcalf <sean@seanmart.com>
2022-01-16 23:36:01 -0500

    added city and state option

--- index.js
+++ index.js
@@ -4,28 +4,40 @@ import fetch from "node-fetch";
 async function getWeather(){
 
   let api_key = getInput('API_KEY')
-  let coordinates = JSON.parse(getInput('COORDINATES'))
+  let coordinates = getInput('COORDINATES')
+  let cityState = getInput('CITY_STATE')
   let exclude = getInput('EXCLUDE')
-  let output = {}
-  let count = 0
+  let props = {}
 
-  if (!Array.isArray(coordinates)) coordinates = [coordinates]
+  if (cityState){
+    cityState = JSON.parse(cityState)
+    props.data = Array.isArray(cityState) ? cityState : [cityState]
+    props.validate = (d)=> !!d.city
+    props.query = (d) => c.state ? `${c.city},${c.state}` : `${c.city}`
+  }
 
-  for (const c of coordinates){
-    let lon = c.longitude || null
-    let lat = c.latitude || null
-    let id = c.id || count
-    let obj = {}
-    if (lon && lat){
-      let query = `lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${api_key}`
-      let res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?${query}`)
-      if (res.ok) output[id] = await res.json()
+  if (coordinates){
+    coordinates = JSON.parse(coordinates)
+    props.data = Array.isArray(coordnates) ? coordnates : [coordnates]
+    props.validate = (d) => d.latitude && d.longitude
+    props.query = (d) => `lat=${d.latitude}&lon=${d.longitude}`
+  }
+
+  if (props && api_key){
+    for (let i = 0; i < props.data){
+      if (props.validate(props.data[i])){
+        let id = props.data.id || i
+        let query = props.query(props.data[i])
+        if (exclude) query += `&exclude=${exclude}`
+        let res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?${query}&appid=${api_key}`)
+        if (res.ok) output[id] = await res.json()
+      }
     }
-    count++
   }
 
   console.log('output',output)
   setOutput('DATA', JSON.stringify(output));
+  
 
 }
 
